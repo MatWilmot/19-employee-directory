038a6fb9aa43f16368057d5adffab346b667b467
Mat Wilmot <matwilmot90@gmail.com>
2020-08-15 16:07:05 -0700

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="jumbotron rounded-0 text-center">
+      <h1 className="display-4">Employee Directory</h1>
+    </div>
+  );
+};
+
+export default Header;
